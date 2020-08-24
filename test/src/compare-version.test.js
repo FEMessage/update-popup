ba64d08f4c33b0b11f9a1c0133785ddad7604b71
evillt <ijoec123@gmail.com>
2020-08-24 21:55:53 +0800

    test: 添加多位数对比版本测试

--- test/src/compare-version.test.js
+++ test/src/compare-version.test.js
@@ -17,3 +17,21 @@ describe('测试版本号对比', () => {
     expect(compareVersion(currentVersion, currentVersion)).toBe(false)
   })
 })
+
+describe('多位数比较', () => {
+  test('4 位数版本 大于 3 位数版本', () => {
+    expect(compareVersion('1.1.0.1', currentVersion)).toBe(true)
+  })
+
+  test('3 位数版本 大于 4 位数版本', () => {
+    expect(compareVersion(currentVersion, '1.0.1.0')).toBe(true)
+  })
+
+  test('新版本 大于 旧版本', () => {
+    expect(compareVersion('1.1.0.1', '1.1.0.0')).toBe(true)
+  })
+
+  test('两版本相同', () => {
+    expect(compareVersion('1.1.0.0', '1.1.0.0')).toBe(false)
+  })
+})
