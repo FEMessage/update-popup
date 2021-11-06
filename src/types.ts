d6c125ac293cda1cc164df04f3c24e862bdff81c
evillt <ijoec123@gmail.com>
2021-11-06 17:48:06 +0800

    feat(types): interface `PupaOptions`

--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,27 @@
+type EnvKey = string
+
 export interface Options {
   /**
-   * Use framework preset by default, set this value to override it.
+   * Use framework preset by default, set this value to override
    * @default ''
    */
   publicBasePath?: string
   /**
    * @default 'UPDATE_POPUP_VERSION'
    */
-  envKey?: string
+  envKey?: EnvKey
   /**
    * @default 'update_popup_version.txt'
    */
   versionFileName?: string
+  /**
+   * @default 'auto'
+   */
   versionType?: 'auto' | 'env'
+}
+
+export interface PupaOptions {
+  envKey: EnvKey
+  currentVersion: string
+  versionFilePath: string
 }
