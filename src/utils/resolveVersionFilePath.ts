ce7b36762f4983550b41f1cb5c61a9008864dc0f
2nthony <ant.hu@outlook.com>
2021-12-30 20:22:16 +0800

    fix(utils): drop `ftp` path resolve
    it doesn't on designed

--- src/utils/resolveVersionFilePath.ts
+++ src/utils/resolveVersionFilePath.ts
@@ -3,7 +3,7 @@ import url from 'url'
 import {normalizePath} from '@rollup/pluginutils'
 
 export function resolveVersionFilePath(publicBasePath: string, to: string) {
-  if (/^((ht|f)tps?:)?\/\//.test(publicBasePath)) {
+  if (/^(https?:)?\/\//.test(publicBasePath)) {
     // use this legacy API is for the `publicBasePath` starts from '//'
     return url.resolve(publicBasePath, to)
   }
